7918c9f1b778d73749141f35f817a91ade7bb64d
Rezkymaulanaa <rezkymailbusiness@gmail.com>
2023-10-04 23:17:30 +0800

    fix: slider image dan detail

--- js/sliderScript.js
+++ js/sliderScript.js
@@ -13,31 +13,31 @@ new Glider(document.querySelector('.glider-per'), {
             breakpoint: 1200,
             settings: {
                 // Set to `auto` and provide item width to adjust to viewport
-                slidesToShow: 4,
+                slidesToShow: 4.5,
                 slidesToScroll: 2,
             }
         }, {
             breakpoint: 900,
             settings: {
-                slidesToShow: 3,
+                slidesToShow: 3.5,
                 slidesToScroll: 1,
             }
         }, {
             breakpoint: 640,
             settings: {
-                slidesToShow: 2,
+                slidesToShow: 3,
                 slidesToScroll: 1,
             }
         }, {
             breakpoint: 304,
             settings: {
-                slidesToShow: 1.5,
+                slidesToShow: 2.5,
                 slidesToScroll: 1,
             }
         }, {
             breakpoint: 0,
             settings: {
-                slidesToShow: 1,
+                slidesToShow: 2.5,
                 slidesToScroll: 1,
             }
         }
@@ -58,31 +58,31 @@ new Glider(document.querySelector('.glider-kel'), {
         {            
             breakpoint: 1200,
             settings: {
-                slidesToShow: 4,
-                slidesToScroll: 2,
+                slidesToShow: 4.5,
+                slidesToScroll: 1,
             }
         }, {
             breakpoint: 900,
             settings: {
-                slidesToShow: 3,
+                slidesToShow: 3.5,
                 slidesToScroll: 1,
             }
         }, {
             breakpoint: 640,
             settings: {
-                slidesToShow: 2,
+                slidesToShow: 3,
                 slidesToScroll: 1,
             }
         }, {
             breakpoint: 304,
             settings: {
-                slidesToShow: 1.5,
+                slidesToShow: 2.5,
                 slidesToScroll: 1,
             }
         }, {
             breakpoint: 0,
             settings: {
-                slidesToShow: 1,
+                slidesToShow: 2.5,
                 slidesToScroll: 1,
             }
         }
